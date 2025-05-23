a143674de0923150bf39ba8982be5b97f3b05fa4
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2025-05-23 16:51:37 +0900

    シーン推移途中

--- Assets/Scenes/Script/SceneManager.cs
+++ Assets/Scenes/Script/SceneManager.cs
@@ -1,27 +1,23 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneScript : MonoBehaviour
 {
-    internal static void LoadScene(string v)
-    {
-        throw new NotImplementedException();
-    }
-
+    
     // Start is called before the first frame update
     void Start()
     {
         
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
-
 }
-
