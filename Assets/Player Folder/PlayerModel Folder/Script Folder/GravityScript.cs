acfe8cd785559f10baf169598762b1f8ae10abc6
ogura715924 <115062896+ogura715924@users.noreply.github.com>
2025-05-23 12:47:10 +0900

    見た目完成

---
+++ Assets/Player Folder/PlayerModel Folder/Script Folder/GravityScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityScript : MonoBehaviour
+{
+    public float gravity = 9.81f;
+    private float verticalVelocity = 0f;
+    public Transform frontWheel;
+    public float rayLength = 0.2f;
+    public LayerMask groundLayer;
+    void Update()
+    {
+        verticalVelocity -= gravity * Time.deltaTime;
+        transform.position += Vector3.up * verticalVelocity * Time.deltaTime;
+    }
+
+    bool IsWheelTouchingGround()
+    {
+        return Physics.Raycast(frontWheel.position, Vector3.down, rayLength, groundLayer);
+    }
+
+
+}
+
