1a0014da21d13c3cbff5f5bec57ebebf1b09e3a1
雄大 渡辺 <veliano393386@gmail.com>
2025-06-02 16:25:05 +0900

    カウントダウンテキスト処理完成

--- Assets/MainGameFolder/GameStartScript.cs
+++ Assets/MainGameFolder/GameStartScript.cs
@@ -35,6 +35,8 @@ public class GameStartScript : MonoBehaviour
     //[Header("カウントダウン用のUI配列")]
     //public GameObject[] countDownUIArray = new GameObject[3];
 
+    // カウントダウンが終了したか
+    private bool endCountDown = false;
 
     // Start is called before the first frame update
     void Start()
@@ -51,61 +53,116 @@ public class GameStartScript : MonoBehaviour
     {
         // デルタタイムを更新
         deltaTime = Time.deltaTime;
-     
+
         // カウントダウンが有効な場合
         if (isStartCountDown)
         {
-            // カウントが最大値と同値の場合(カウント開始時や減少時)
-            // カウントUI(またはログ)の表示を行う
-            if (currenOneSecond == oneSecond)
-            {
-                // カウントダウンのテキストを有効化
-                countDownText.gameObject.SetActive(true);
+            // カウントダウンの実行
+            UpdateCountDown();
+        }
 
-                // (ToDo)UIの表示を行い、更新を有効にする
-                countDownText.text = countDownTime.ToString();
+        if (endCountDown)
+        {
 
-                // デバッグ用のログ出力
-                Debug.Log("カウントダウン: " + countDownTime + "秒");
-            }
+            // テキストの拡大率を上昇
+            countDownText.transform.localScale = new Vector3(
+                countDownText.transform.localScale.x + (deltaTime * 24.0f),
+                countDownText.transform.localScale.y + (deltaTime * 24.0f),
+                countDownText.transform.localScale.z
+                );
+
+            // テキストを移動
+            countDownText.transform.Translate(new Vector3(0.0f, deltaTime * 600.0f, 0.0f));
 
 
-           
-
-
-            // 経過時間を更新
-            currenOneSecond -= deltaTime;
-
-            // 1秒経過したらカウントダウンを進める
-            if (currenOneSecond <= 0.0f)
-            {
-
-                // カウントダウン時間を減らす
-                countDownTime--;
-                
-                // 次の1秒の時間を設定
-                currenOneSecond = oneSecond;
-
-                // カウントダウンが終了した場合
-                if (countDownTime <= 0)
-                {
-                    // カウントダウンのテキストを更新
-                    countDownText.text = "GO!";
-
-                    // デバッグ用のログ出力
-                    Debug.Log("行きましょう");
-
-                    // カウントダウンを無効にする
-                    isStartCountDown = false;
-                    // ゲーム開始の処理を行う
-                    //StartGame();
-                }
-
-               
-
-            }
-
+            // カウントダウンが終了した場合の処理
+            // コルーチンを使用し、0.5秒後にカウントダウンのテキストを非表示にする
+            StartCoroutine(EndCountDoun());
         }
 
     }
+
+    // カウントダウン処理
+    void UpdateCountDown()
+    {
+        // カウントが最大値と同値の場合(カウント開始時や減少時)
+        // カウントUI(またはログ)の表示を行う
+        if (currenOneSecond == oneSecond)
+        {
+            // カウントダウンのテキストを有効化
+            countDownText.gameObject.SetActive(true);
+            // テキストの拡大率をリセット
+            countDownText.transform.localScale = Vector3.one;
+            // テキストの回転をリセット
+            countDownText.transform.rotation = Quaternion.identity;
+
+            // (ToDo)UIの表示を行い、更新を有効にする
+            // カウントダウンの残り秒数の値に合わせて表示を更新
+            countDownText.text = countDownTime.ToString();
+
+            // デバッグ用のログ出力
+            Debug.Log("カウントダウン: " + countDownTime + "秒");
+        }
+
+        // テキストの拡大率を減少
+        countDownText.transform.localScale = new Vector3(
+            countDownText.transform.localScale.x - (deltaTime * 1.0f),
+            countDownText.transform.localScale.y - (deltaTime * 1.0f),
+            countDownText.transform.localScale.z
+            );
+        // テキストを回転
+        countDownText.transform.Rotate(new Vector3(0.0f, 0.0f, deltaTime * 360.0f));
+
+        // 経過時間を更新
+        currenOneSecond -= deltaTime;
+
+        // 1秒経過したらカウントダウンを進める
+        if (currenOneSecond > 0.0f)
+        {
+            // 経過していない場合は早期リターン
+            return;
+        }
+
+        // カウントダウン時間を減らす
+        countDownTime--;
+        // 次の1秒の時間を設定
+        currenOneSecond = oneSecond;
+
+
+        // カウントダウンが終了した場合
+        if (countDownTime <= 0)
+        {
+            // カウントダウンのテキストを更新
+            countDownText.text = "GO!";
+            // テキストの拡大率をリセット
+            countDownText.transform.localScale = Vector3.one;
+            // テキストの回転をリセット
+            countDownText.transform.rotation = Quaternion.identity;
+
+            // デバッグ用のログ出力
+            Debug.Log("行きましょう");
+
+            // カウントダウンを無効にする
+            isStartCountDown = false;
+            // カウントダウンが終了したフラグを立てる
+            endCountDown = true;
+            // ゲーム開始の処理を行う
+            //StartGame();
+        }
+
+
+    }
+
+    IEnumerator EndCountDoun()
+    {
+
+        // カウント終了後の後処理
+        // 0.5秒後にカウントダウンのテキストを非表示にする
+        yield return new WaitForSeconds(0.5f);
+        // カウントダウンのテキストを無効化
+        countDownText.gameObject.SetActive(false);
+
+
+    }
+
 }
