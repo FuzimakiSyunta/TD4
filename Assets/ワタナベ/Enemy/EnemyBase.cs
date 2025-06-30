ad9623f4a3f502aeb54a2eb51c15fbcb804108c0
雄大 渡辺 <veliano393386@gmail.com>
2025-06-30 16:50:10 +0900

    生成処理 一旦完成。 EnemyDataが完全に埋められるようになったら再度いじること(ステージ完成後など)

--- Assets/ワタナベ/Enemy/EnemyBase.cs
+++ Assets/ワタナベ/Enemy/EnemyBase.cs
@@ -6,10 +6,11 @@ public class EnemyBase : MonoBehaviour
 {
 
     // 敵ステータス管理(性格・バイクの性能・体力・運転能力などのステータスを取得する)
-
+    EnemyData enemyData;
 
     // 敵のルート管理(分岐や、ルート内の左右移動や加速の指示を行う)
-
+    [Header("ルート制御 スクリプト")]
+    public RouteController routeController;
 
     // 敵スタント管理(上記のステータスやルート情報から行うスタントを決定する)
 
@@ -33,6 +34,16 @@ public class EnemyBase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // ルート制御スクリプトが設定されている場合は、ルート制御の更新を行う
+        if (routeController != null) {
+            // ルート制御の更新処理を呼び出す
+            routeController.Invoke("Update", Time.deltaTime);
+            // ルート制御スクリプトから移動量を取得して、敵の位置を更新する
+            Vector3 moveDirection = routeController.transform.forward; // 仮の移動方向を取得
+            float moveSpeed = 30f; // 仮の移動速度を設定
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+        }
+
         // 仮で  自身を前方に移動する
         transform.Translate(Vector3.forward * Time.deltaTime * 30f);
 
